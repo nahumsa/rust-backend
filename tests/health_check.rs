5520590ea7a8b16c6fb6fcd61d5a3513773fef24
Nahum Sá <nahumsaa@gmail.com>
2023-04-17 19:57:35 -0300

    refactored code to receive a TCPListiner when starting the API

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,12 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn test_health_check() {
-    start_app();
-
+    let address: String = start_app();
     let client = reqwest::Client::new();
+
     let response = client
-        .get("http://127.0.01:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -13,9 +15,15 @@ async fn test_health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn start_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn start_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind a random port");
+
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 
+    format!("http://127.0.1:{}", port)
 }
