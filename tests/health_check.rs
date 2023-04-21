5822a551474d0e758069d445c7954b6e68a6c202
Nahum Sá <nahumsaa@gmail.com>
2023-04-21 14:04:55 -0300

    add tests for /subscription Fixes #1

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,6 +15,54 @@ async fn test_health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn test_subscribe_return_200_for_valid_form_data() {
+    let address: String = start_app();
+
+    let client = reqwest::Client::new();
+
+    let body = "name=nahum%20sa&email=nahumsa%40email.com";
+
+    let response = client
+        .post(&format!("{}/subscription", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn test_subscribe_return_400_when_data_is_missing() {
+    let address: String = start_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=nahum%20sa", "missing the email"),
+        ("email=nahumsa@email.com", "missing the name"),
+        ("", "missing email and name"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscription", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    };
+}
+
 fn start_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind a random port");
 
