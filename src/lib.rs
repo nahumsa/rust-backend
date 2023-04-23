fbf83a0b52b47d03f30452eee74fe9a6eecf4542
Nahum Sá <nahumsaa@gmail.com>
2023-04-23 19:12:14 -0300

    Reconfigure file structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,13 @@
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use routes::{health_check, subscribe};
 
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
