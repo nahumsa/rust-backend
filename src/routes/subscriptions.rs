c733ce96e68bce9169059e28c8513407629947b7
Nahum Sá <nahumsaa@gmail.com>
2023-04-25 22:16:03 -0300

    Fix test failing and wrong script path Fixes #1

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,19 +13,19 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
+        VALUES ($1, $2, $3, $4);
         "#,
         Uuid::new_v4(),
         form.email,
         form.name,
-        Utc::now()
+        Utc::now(),
     )
-    .execute(pool.get_ref())
+    .execute(pool.as_ref())
     .await
     {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            println!("Failed to execute query: {e}");
             HttpResponse::InternalServerError().finish()
         }
     }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,84 +5,6 @@ use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 
-#[tokio::test]
-async fn test_health_check() {
-    let app: TestApp = start_app().await;
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(&format!("{}/health_check", &app.address))
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
-}
-
-#[tokio::test]
-async fn test_subscribe_return_200_for_valid_form_data() {
-    // setup
-    let app: TestApp = start_app().await;
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_string = configuration.database.connection_string();
-
-    let mut connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to Postgres");
-
-    let client = reqwest::Client::new();
-    let body = "name=nahum%20sa&email=nahumsa%40email.com";
-
-    let response = client
-        .post(&format!("{}/subscriptions", &app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request");
-
-    assert_eq!(200, response.status().as_u16());
-
-    // Verify if the data was persisted in the database
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions;",)
-        .fetch_one(&mut connection)
-        .await
-        .expect("Failed to fetch saved subscription.");
-
-    assert_eq!(saved.email, "nahumsa@email.com");
-    assert_eq!(saved.name, "nahum sa");
-}
-
-#[tokio::test]
-async fn test_subscribe_return_400_when_data_is_missing() {
-    let app: TestApp = start_app().await;
-    let client = reqwest::Client::new();
-
-    let test_cases = vec![
-        ("name=nahum%20sa", "missing the email"),
-        ("email=nahumsa@email.com", "missing the name"),
-        ("", "missing email and name"),
-    ];
-
-    for (invalid_body, error_message) in test_cases {
-        let response = client
-            .post(&format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request");
-
-        assert_eq!(
-            400,
-            response.status().as_u16(),
-            "The API did not fail with 400 Bad Request when the payload was {}",
-            error_message
-        );
-    }
-}
-
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
@@ -128,4 +50,76 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to migrate the database");
 
     connection_pool
+}
+
+#[tokio::test]
+async fn test_health_check() {
+    let app: TestApp = start_app().await;
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &app.address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn test_subscribe_return_200_for_valid_form_data() {
+    // setup
+    let app: TestApp = start_app().await;
+
+    let client = reqwest::Client::new();
+    let body = "name=nahum%20sa&email=nahumsa%40email.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+
+    // Verify if the data was persisted in the database
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions;",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "nahumsa@email.com");
+    assert_eq!(saved.name, "nahum sa");
+}
+
+#[tokio::test]
+async fn test_subscribe_return_400_when_data_is_missing() {
+    let app: TestApp = start_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=nahum%20sa", "missing the email"),
+        ("email=nahumsa@email.com", "missing the name"),
+        ("", "missing email and name"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
 }
