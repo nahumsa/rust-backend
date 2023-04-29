85582b197a42f77a85209f5bea50d7152c511fc2
Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>
2023-04-29 13:01:23 -0300

    update init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -14,29 +14,36 @@ if ! [ -x "$(command -v sqlx)" ]; then
     echo >&2 "to install it."
     exit 1
 fi
-DB_USER=${POSTGRES_USER:=postgres}
+DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Skip docker by setting `SKIP_DOCKER=true` when running the bash file
+# Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-# Launch postgres using Docker
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
-
+  # if a postgres container is running, print instructions to kill it and exit
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "there is a postgres container already running, kill it with"
+    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+    exit 1
+  fi
+  # Launch postgres using Docker
+  docker run \
+      -e POSTGRES_USER=${DB_USER} \
+      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+      -e POSTGRES_DB=${DB_NAME} \
+      -p "${DB_PORT}":5432 \
+      -d \
+      --name "postgres_$(date '+%s')" \
+      postgres -N 1000
+      # ^ Increased maximum number of connections for testing purposes
 fi
 
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "localhost" -U "$DB_USER" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-    >&2 echo "Postgres is still unavailable - sleeping"
-    sleep 1
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
 done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}."
