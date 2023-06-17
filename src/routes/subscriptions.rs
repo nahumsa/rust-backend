9a3f81466019cce592b435afa791e2a60fb6e863
Nahum Sá <nahumsaa@gmail.com>
2023-06-17 00:01:07 -0300

    add NewSubscriber type

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,3 +1,4 @@
+use crate::domain::NewSubscriber;
 use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -27,7 +28,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
     name = "Saving new subscriber details in the database",
     skip(form, pool)
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(pool: &PgPool, form: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
