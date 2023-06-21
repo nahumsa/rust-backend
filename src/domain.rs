469c7429e9503dabf12a1b33f4b8741659bd605c
Nahum Sá <nahumsaa@gmail.com>
2023-06-20 21:27:22 -0300

    add invariants for SubscriberName

--- src/domain.rs
+++ src/domain.rs
@@ -1,16 +1,15 @@
-use unicode_segmentation::UnicodeSegmentation;
-
 pub struct NewSubscriber {
     pub email: String,
     pub name: SubscriberName,
 }
 
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl SubscriberName {
     pub fn parse(s: String) -> SubscriberName {
         let is_empty_or_whitespace = s.trim().is_empty();
-        let is_too_long = s.graphmemes(true).count() > 256;
+        let is_too_long = s.chars().count() > 256;
 
         let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
@@ -20,5 +19,14 @@ impl SubscriberName {
         } else {
             Self(s)
         }
+    }
+    pub fn inner(self) -> String {
+        self.0
+    }
+    pub fn inner_mut(&mut self) -> &mut str {
+        &mut self.0
+    }
+    pub fn inner_ref(&self) -> &str {
+        &self.0
     }
 }
