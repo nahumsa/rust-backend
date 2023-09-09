5aedd8fb551d6e882010d1d3d9dab4dd9856c7fb
Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>
2023-09-09 16:54:06 -0300

    Create validation for subscription names and emails (#14)
    * add NewSubscriber type
    
    * add test for invalid inputs
    
    * update cargo toml
    
    * add invariants for SubscriberName
    
    * add validation for names
    
    * create domain module
    
    * add validation for emails
    
    * add email validation for the request
    
    * refactor using TryFrom
    
    * fix clippy errors

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -29,7 +29,7 @@ async fn start_app() -> TestApp {
 
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind a random port");
     let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.1:{}", port);
+    let address = format!("http://127.0.1:{port}");
 
     let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
@@ -112,6 +112,34 @@ async fn test_subscribe_return_200_for_valid_form_data() {
 }
 
 #[tokio::test]
+async fn test_subscribe_returns_400_when_fields_are_present_but_invalid() {
+    // setup
+    let app: TestApp = start_app().await;
+
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=not-an-email", "invalid email"),
+    ];
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {description}"
+        );
+    }
+}
+
+#[tokio::test]
 async fn test_subscribe_return_400_when_data_is_missing() {
     let app: TestApp = start_app().await;
     let client = reqwest::Client::new();
@@ -134,8 +162,7 @@ async fn test_subscribe_return_400_when_data_is_missing() {
         assert_eq!(
             400,
             response.status().as_u16(),
-            "The API did not fail with 400 Bad Request when the payload was {}",
-            error_message
+            "The API did not fail with 400 Bad Request when the payload was {error_message}"
         );
     }
 }
